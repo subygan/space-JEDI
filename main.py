f910f7864b151673417b67d773aee966a326c7ca
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2023-09-16 10:22:45 -0400

    update

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ max_distance = st.sidebar.slider('Max Distance Traveled (km)', 10, 500, 100)
 starting_point = st.sidebar.selectbox('Select Starting Point', ['Point A', 'Point B', 'Point C'])
 weight_constraint = st.sidebar.number_input('Weight Constraint (kg)', min_value=0, value=50)
 
-components.html('''
-<h1> Trip </h1>''')
+
+st.title('Trip Planner App')
 
 st.write('')
 
@@ -19,9 +19,10 @@ components.html('''
   style="width:100%;border:none;"
 ></iframe>''', height=600)
 
-st.write('### User Inputs')
-st.write(f'- Max Time for Trip: {max_time} hours')
-st.write(f'- Max Distance Traveled: {max_distance} km')
-st.write(f'- Starting Point: {starting_point}')
-st.write(f'- Weight Constraint: {weight_constraint} kg')
-st.write('Check out your scheduled plan: http://127.0.0.1/')
+if st.sidebar.button("Submit"):
+  st.write('### User Inputs')
+  st.write(f'- Max Time for Trip: {max_time} hours')
+  st.write(f'- Max Distance Traveled: {max_distance} km')
+  st.write(f'- Starting Point: {starting_point}')
+  st.write(f'- Weight Constraint: {weight_constraint} kg')
+  st.write('Check out your scheduled plan: http://127.0.0.1/')
