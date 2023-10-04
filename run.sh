208bc4274ac034e0983761568b651be3cc1849a1
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2023-10-04 15:41:37 -0400

    shell script

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if the operating system is Windows or macOS
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    # Windows
+    VENV_DIR="myenv/Scripts/activate"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    VENV_DIR="myenv/bin/activate"
+else
+    echo "Unsupported operating system."
+    exit 1
+fi
+
+# Activate the virtual environment
+source "$VENV_DIR"
+
+# Run the streamlit command
+streamlit run app.py
